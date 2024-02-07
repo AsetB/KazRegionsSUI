f2749a90655a964626105cbd18b2b4fcf921a470
AsetB <asetbakirov@gmail.com>
2024-02-07 13:26:08 +0600

    Initial Commit

---
+++ KazRegionsSUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KazRegionsSUI
+//
+//  Created by Aset Bakirov on 07.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KazRegionsSUI/KazRegionsSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KazRegionsSUIApp.swift
+//  KazRegionsSUI
+//
+//  Created by Aset Bakirov on 07.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct KazRegionsSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
